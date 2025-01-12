d2e809ebf0d4e6c74e7bc9ec5ad7f925fdb41cda
lalanabiyaa <octavianinabilla123@gmail.com>
2025-01-12 23:58:09 +0700

    perbaikan

--- js/script.js
+++ js/script.js
@@ -1,64 +1,75 @@
-const donutColors = [0xaec6cf, 0xff6347, 0x333333, 0xffddc1, 0xd2691e];
+const donutColors = [0xaec6cf, 0xa2262e, 0x2b1700, 0xffddc1, 0xd2691e];
 
 class DonutRain {
-	constructor() {
-		this.donuts = [];
-		this.numDonuts = 30;
-		this.donutColorIndex = 0;
-	}
+   constructor() {
+      this.donuts = [];
+      this.numDonuts = 30;
+      this.donutColorIndex = 0;
+      this.textureLoader = new THREE.TextureLoader();
+      this.donutTexture = this.textureLoader.load(
+         'assets/donut/textures/donut_normal.png'
+      );
+   }
 
-	init() {
-		this.createDonuts();
-		this.animateDonuts();
-	}
+   init() {
+      this.createDonuts();
+      this.animateDonuts();
+   }
 
-	createDonuts() {
-		for (let i = 0; i < this.numDonuts; i++) {
-			const donutGeometry = new THREE.TorusGeometry(0.5, 0.2, 16, 100);
-			const donutMaterial = new THREE.MeshBasicMaterial({ color: donutColors[this.donutColorIndex] });
-			const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+   createDonuts() {
+      for (let i = 0; i < this.numDonuts; i++) {
+         const donutGeometry = new THREE.TorusGeometry(0.5, 0.2, 16, 100);
+         const donutMaterial = new THREE.MeshStandardMaterial({
+            color: donutColors[this.donutColorIndex],
+            normalMap: this.donutTexture,
+         });
+         const donut = new THREE.Mesh(donutGeometry, donutMaterial);
 
-			donut.position.set(
-				Math.random() * 40 - 5, // x
-				Math.random() * 10 + 10, // y
-				Math.random() * 5 - 10 // z
-			);
-			donut.rotation.set(Math.random(), Math.random(), Math.random());
+         donut.position.set(
+            Math.random() * 40 - 5, // x
+            Math.random() * 10 + 10, // y
+            Math.random() * 5 - 10 // z
+         );
+         donut.rotation.set(Math.random(), Math.random(), Math.random());
 
-			this.donuts.push(donut);
-			scene.add(donut);
-		}
-	}
+         this.donuts.push(donut);
+         scene.add(donut);
+      }
+   }
 
-	animateDonuts() {
-		const animate = () => {
-			for (let donut of this.donuts) {
-				donut.position.y -= 0.02;
-				donut.rotation.x += 0.005;
-				donut.rotation.y += 0.005;
+   animateDonuts() {
+      const animate = () => {
+         for (let donut of this.donuts) {
+            donut.position.y -= 0.02;
+            donut.rotation.x += 0.005;
+            donut.rotation.y += 0.005;
 
-				if (donut.position.y < -5) {
-					donut.position.y = Math.random() * 10 + 10;
-				}
-			}
-			requestAnimationFrame(animate);
-		};
-		animate();
-	}
+            if (donut.position.y < -5) {
+               donut.position.y = Math.random() * 10 + 10;
+            }
+         }
+         requestAnimationFrame(animate);
+      };
+      animate();
+   }
 
-	updateDonutColor(index) {
-		this.donutColorIndex = index;
-		console.log(`Donut color updated to index: ${index}, Color: ${donutColors[this.donutColorIndex].toString(16)}`);
-		for (let donut of this.donuts) {
-			donut.material.color.set(donutColors[this.donutColorIndex]);
-		}
-	}
+   updateDonutColor(index) {
+      this.donutColorIndex = index;
+      console.log(
+         `Donut color updated to index: ${index}, Color: ${donutColors[
+            this.donutColorIndex
+         ].toString(16)}`
+      );
+      for (let donut of this.donuts) {
+         donut.material.color.set(donutColors[this.donutColorIndex]);
+      }
+   }
 }
 
 /**
  * Base
  */
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
@@ -67,18 +78,21 @@ const scene = new THREE.Scene();
 let donut = null;
 
 const loadModel = () => {
-	if (donut) {
-		scene.remove(donut);
-	}
+   if (donut) {
+      scene.remove(donut);
+   }
 
-	const geometry = new THREE.TorusGeometry(0.6, 0.3, 128, 128);
-	const material = new THREE.MeshPhongMaterial({ color: 0x93471b, shininess: 100 });
+   const geometry = new THREE.TorusGeometry(0.6, 0.3, 128, 128);
+   const material = new THREE.MeshPhongMaterial({
+      color: 0x93471b,
+      shininess: 100,
+   });
 
-	donut = new THREE.Mesh(geometry, material);
-	donut.position.set(1.5, 5, 0);
-	donut.rotation.x = Math.PI * -1.2;
-	donut.rotation.z = Math.PI * -0.15;
-	scene.add(donut);
+   donut = new THREE.Mesh(geometry, material);
+   donut.position.set(1.5, 20, 0); // Pastikan posisi tidak bertabrakan
+   donut.rotation.x = Math.PI * -1.2;
+   donut.rotation.z = Math.PI * -0.15;
+   scene.add(donut);
 };
 
 loadModel();
@@ -97,11 +111,16 @@ scene.add(pointLight);
  * Sizes
  */
 const sizes = {
-	width: window.innerWidth,
-	height: window.innerHeight,
+   width: window.innerWidth,
+   height: window.innerHeight,
 };
 
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+   35,
+   sizes.width / sizes.height,
+   0.1,
+   1000
+);
 camera.position.z = 5;
 scene.add(camera);
 
@@ -109,9 +128,9 @@ scene.add(camera);
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-	canvas: canvas,
-	antialias: true,
-	alpha: true,
+   canvas: canvas,
+   antialias: true,
+   alpha: true,
 });
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -125,43 +144,67 @@ gsap.registerPlugin(ScrollTrigger);
 
 let currentSection = 0;
 const transformDonut = [
-	{ rotationZ: 0.45, positionX: 1.5 },
-	{ rotationZ: -0.45, positionX: -1.5 },
-	{ rotationZ: 0.0314, positionX: 0 },
+   { rotationZ: 0.45, positionX: 1.5 },
+   { rotationZ: -0.45, positionX: -1.5 },
+   { rotationZ: 0.0314, positionX: 0 },
 ];
 
 // Create scroll triggers for each section
 ScrollTrigger.create({
-	trigger: ".one",
-	start: "top top",
-	end: "bottom top",
-	onEnter: () => {
-		currentSection = 0;
-		gsap.to(donut.rotation, { duration: 1.5, ease: "power2.inOut", z: transformDonut[0].rotationZ });
-		gsap.to(donut.position, { duration: 1.5, ease: "power2.inOut", x: transformDonut[0].positionX });
-	},
+   trigger: '.one',
+   start: 'top top',
+   end: 'bottom top',
+   onEnter: () => {
+      currentSection = 0;
+      gsap.to(donut.rotation, {
+         duration: 1.5,
+         ease: 'power2.inOut',
+         z: transformDonut[0].rotationZ,
+      });
+      gsap.to(donut.position, {
+         duration: 1.5,
+         ease: 'power2.inOut',
+         x: transformDonut[0].positionX,
+      });
+   },
 });
 
 ScrollTrigger.create({
-	trigger: ".two",
-	start: "top top",
-	end: "bottom top",
-	onEnter: () => {
-		currentSection = 1;
-		gsap.to(donut.rotation, { duration: 1.5, ease: "power2.inOut", z: transformDonut[1].rotationZ });
-		gsap.to(donut.position, { duration: 1.5, ease: "power2.inOut", x: transformDonut[1].positionX });
-	},
+   trigger: '.two',
+   start: 'top top',
+   end: 'bottom top',
+   onEnter: () => {
+      currentSection = 1;
+      gsap.to(donut.rotation, {
+         duration: 1.5,
+         ease: 'power2.inOut',
+         z: transformDonut[1].rotationZ,
+      });
+      gsap.to(donut.position, {
+         duration: 1.5,
+         ease: 'power2.inOut',
+         x: transformDonut[1].positionX,
+      });
+   },
 });
 
 ScrollTrigger.create({
-	trigger: ".three",
-	start: "top top",
-	end: "bottom top",
-	onEnter: () => {
-		currentSection = 2;
-		gsap.to(donut.rotation, { duration: 1.5, ease: "power2.inOut", z: transformDonut[2].rotationZ });
-		gsap.to(donut.position, { duration: 1.5, ease: "power2.inOut", x: transformDonut[2].positionX });
-	},
+   trigger: '.three',
+   start: 'top top',
+   end: 'bottom top',
+   onEnter: () => {
+      currentSection = 2;
+      gsap.to(donut.rotation, {
+         duration: 1.5,
+         ease: 'power2.inOut',
+         z: transformDonut[2].rotationZ,
+      });
+      gsap.to(donut.position, {
+         duration: 1.5,
+         ease: 'power2.inOut',
+         x: transformDonut[2].positionX,
+      });
+   },
 });
 
 /**
@@ -171,16 +214,16 @@ const clock = new THREE.Clock();
 let lastElapsedTime = 0;
 
 const tick = () => {
-	const elapsedTime = clock.getElapsedTime();
-	const deltaTime = elapsedTime - lastElapsedTime;
-	lastElapsedTime = elapsedTime;
+   const elapsedTime = clock.getElapsedTime();
+   const deltaTime = elapsedTime - lastElapsedTime;
+   lastElapsedTime = elapsedTime;
 
-	if (donut) {
-		donut.position.y = Math.sin(elapsedTime * 0.5) * 0.1 - 0.1;
-	}
+   if (donut) {
+      donut.position.y = Math.sin(elapsedTime * 0.5) * 0.1 - 0.1;
+   }
 
-	renderer.render(scene, camera);
-	window.requestAnimationFrame(tick);
+   renderer.render(scene, camera);
+   window.requestAnimationFrame(tick);
 };
 
 const donutRain = new DonutRain();
@@ -192,17 +235,17 @@ tick();
  * On Reload
  */
 window.onbeforeunload = function () {
-	window.scrollTo(0, 0);
+   window.scrollTo(0, 0);
 };
 
 /**
  * Resize Event
  */
-window.addEventListener("resize", () => {
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
+window.addEventListener('resize', () => {
+   sizes.width = window.innerWidth;
+   sizes.height = window.innerHeight;
 
-	renderer.setSize(sizes.width, sizes.height);
-	camera.aspect = sizes.width / sizes.height;
-	camera.updateProjectionMatrix();
+   renderer.setSize(sizes.width, sizes.height);
+   camera.aspect = sizes.width / sizes.height;
+   camera.updateProjectionMatrix();
 });
